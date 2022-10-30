825224dd74b8a694ef29608f40cdf3d17845a795
Daniel Grayson <graysoncodes@gmail.com>
2022-10-30 11:20:54 +0100

    added more updated to web layout

--- lib/screens/mobile_layout.dart
+++ lib/screens/mobile_layout.dart
@@ -6,34 +6,34 @@ class MobileLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-              Stack(children: [
-                ShaderMask(
-                  shaderCallback: (bounds) => const RadialGradient(
-                    colors: [Colors.black12, Colors.black45],
-                  ).createShader(bounds),
-                  blendMode: BlendMode.darken,
-                  child: AspectRatio(
-                    aspectRatio: 3 / 2,
-                    child: Container(
-                        width: MediaQuery.of(context).size.width * 0.99999,
-                        height: 70,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("backgroundImage1.jpg"),
-                              fit: BoxFit.fill,
-                              colorFilter: ColorFilter.mode(
-                                Colors.black45,
-                                BlendMode.darken,
-                              )),
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Stack(children: [
+            ShaderMask(
+              shaderCallback: (bounds) => const RadialGradient(
+                colors: [Colors.black12, Colors.black45],
+              ).createShader(bounds),
+              blendMode: BlendMode.darken,
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.70,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("backgroundImage1.jpg"),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                          Colors.black45,
+                          BlendMode.darken,
                         )),
-                  ),
-                ),
-              ])
-            ])));
+                  )),
+            ),
+            Column(
+              children: [],
+            )
+          ])
+        ]),
+      ),
+    );
   }
 }
--- lib/screens/web_layout.dart
+++ lib/screens/web_layout.dart
@@ -19,21 +19,18 @@ class WebLayout extends StatelessWidget {
                       colors: [Colors.black12, Colors.black45],
                     ).createShader(bounds),
                     blendMode: BlendMode.darken,
-                    child: AspectRatio(
-                      aspectRatio: 3 / 2,
-                      child: Container(
-                          // width: MediaQuery.of(context).size.width * 0.99999,
-                          height: 400,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("backgroundImage1.jpg"),
-                                fit: BoxFit.fill,
-                                colorFilter: ColorFilter.mode(
-                                  Colors.black45,
-                                  BlendMode.darken,
-                                )),
-                          )),
-                    ),
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.95,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("backgroundImage1.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(
+                                Colors.black45,
+                                BlendMode.darken,
+                              )),
+                        )),
                   ),
                   Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
